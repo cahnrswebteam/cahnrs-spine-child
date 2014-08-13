ee5cbb76ef0930103f270cf46cc6bc667693169d
Phil Cable <pcable@wsu.edu>
2014-08-13 15:26:47 -0700

    Added 404.php
    Leverage later to allow for customizable 404 pages

---
+++ 404.php
@@ -0,0 +1,29 @@
+<?php get_header(); ?>
+
+<main class="spine-single-template">
+
+	<?php get_template_part('parts/headers'); ?>
+
+  <section class="row single">
+
+    <div class="column one">
+
+      <article id="post-0" class="post error404 no-results not-found">
+
+        <header class="article-header">
+          <h1 class="article-title">Not Found</h1>
+        </header>
+
+        <div class="entry-content">
+          <p>The CAHNRS and Extension web presence is under reconstruction. We apologize for the inconvenience and appreciate your patience during this transition. The content you are looking is most likely in a new location, so please try using the navigation or the search form below to find it.</p>
+          <?php get_search_form(); ?>
+        </div><!-- .entry-content -->
+
+      </article>
+
+    </div><!--/column-->
+
+  </section>
+</main>
+
+<?php get_footer(); ?>
