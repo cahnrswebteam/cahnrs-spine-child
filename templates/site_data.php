83223141f9be2beb32a3a1bf93a937161f3e63bd
danialbleile <danial.bleile@wsu.edu>
2014-08-27 12:38:53 -0700

    Add Site Data Feed

--- templates/site_data.php
+++ templates/site_data.php
@@ -3,9 +3,9 @@ class cahnrs_json_site_data {
 	public $site_data = array();
 	
 	public function __construct(){
-		$site_data['menu'] = $this->get_menu();
-		$site_data['page'] = $this->get_page();
-		print_r( $site_data );
+		$this->site_data['menu'] = $this->get_menu();
+		$this->site_data['page'] = $this->get_page();
+		$this->site_data['background'] = $this->get_background();
 	}
 	
 	public function get_menu(){
@@ -22,11 +22,19 @@ class cahnrs_json_site_data {
 		$page = '';
 		if( \has_nav_menu( 'site' ) ){
 			ob_start();
-			include 'm1.wpdev.cahnrs.wsu.edu';
+			include( get_stylesheet_directory().'/front-page.php');
 			$page = ob_get_clean();
 		}
 		return $page;
 	}
+	
+	public function get_background(){
+		return 'http://stage.wpdev.cahnrs.wsu.edu/research/wp-content/uploads/sites/9/2014/08/DSC_9724.jpg';
+	}
+	public function get_json() {
+		echo json_encode( $this->site_data );
+	}
 }
 $init_cahnrs_json_site_data = new cahnrs_json_site_data();
+$init_cahnrs_json_site_data->get_json();
 ?>
