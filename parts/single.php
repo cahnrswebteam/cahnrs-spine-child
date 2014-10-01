a5fe30281c20a76575523af077054a387a54ddfd
danialbleile <danial.bleile@wsu.edu>
2014-10-01 05:33:21 -0700

    Add Subheading Code

--- parts/single.php
+++ parts/single.php
@@ -1,8 +1,17 @@
-<?php // Just a stub for now ?>
-		
+<?php // Just a stub for now 
+global $post;
+$subhead = false;
+if( is_front_page()){
+	$meta = get_post_meta( $post->ID , '_post_subtitle', true );
+	if( $meta ) $subhead = $meta;
+}
+?>		
 <article id="post-<?php the_ID(); ?>" class="post-<?php the_ID(); ?> primary-article" <?php /* post_class();*/ ?> >
 	<header class="article-header">
     <h1 class="article-title"><?php the_title(); ?></h1>
+    <?php if( $subhead ):?> 
+    <div class="article-subtitle"><?php echo $subhead;?></div>
+    <?php endif;?>
 	</header>
 	<?php the_content(); ?>
 </article>
